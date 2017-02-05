3a4f42e914935bc7572508fbd4a0d7d784cac3ce
Karel Bemelmans <karel@narfum.org>
2017-02-05 21:40:19 +0100

    Initial commit

---
+++ drupal-local.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# This script fires up the development environment with the mounted Drupal
+# directory.
+
+COMPOSE_FILE=./docker-compose-dev.yml
+
+case "$1" in
+
+  up)
+    echo "Building Docker environment..."
+    docker-compose -f $COMPOSE_FILE build --pull
+
+    echo "Starting Docker environment..."
+    docker-compose -f $COMPOSE_FILE up -d
+    ;;
+
+  start)
+    echo "Building Docker environment..."
+    docker-compose -f $COMPOSE_FILE build --pull
+
+    echo "Destroying Docker environment..."
+    docker-compose -f $COMPOSE_FILE start
+    ;;
+
+  stop)
+    echo "Stopping Docker environment..."
+    docker-compose -f $COMPOSE_FILE stop
+    ;;
+
+  destroy)
+    if [ "$2" = "--really" ]; then
+      echo "Destroying Docker environment..."
+      docker-compose -f $COMPOSE_FILE down
+      exit 0
+    fi
+
+    echo "###################################################################"
+    echo "#                     !!!DANGER ALERT !!!                         #"
+    echo "###################################################################"
+    echo "You need to run this command with the '--really' option to proceed."
+    ;;
+
+  *)
+    echo "Usage: $0 up|start|down|stop"
+    ;;
+esac
+
+
+
